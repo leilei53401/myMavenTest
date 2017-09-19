a6c948ea83d9793a05e9d9f0ab7c175d045bc49e
shaoyl <shaoyl@ivoole.com>
2017-09-19 11:46:40 +0800

    add

---
+++ src/main/java/test/GitTest.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * Created by Administrator on 2017-9-19.
+ */
+public class GitTest {
+    public static void main(String[] args){
+        try {
+            System.out.println("hello git!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
