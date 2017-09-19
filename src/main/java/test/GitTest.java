ffdb24f15b20127c5334a4d4dfef21717444b681
shaoyl <shaoyl@ivoole.com>
2017-09-19 11:50:08 +0800

    dev add 1

--- src/main/java/test/GitTest.java
+++ src/main/java/test/GitTest.java
@@ -7,6 +7,8 @@ public class GitTest {
     public static void main(String[] args){
         try {
             System.out.println("hello git!");
+            
+            System.out.println("test2");
         } catch (Exception e) {
             e.printStackTrace();
         }
